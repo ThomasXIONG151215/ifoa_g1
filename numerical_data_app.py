4914c07c120a29e74477bdf2bff5360f18b385bd
Yuanke <60814001+ThomasXIONG151215@users.noreply.github.com>
2024-10-29 00:19:03 +0800

    Update numerical_data_app.py

--- numerical_data_app.py
+++ numerical_data_app.py
@@ -136,8 +136,8 @@ def data_viewer(df):
           Q1 = series.quantile(0.25)
           Q3 = series.quantile(0.75)
           IQR = Q3 - Q1
-          lower_bound = Q1 - 1.5 * IQR
-          upper_bound = Q3 + 1.5 * IQR
+          lower_bound = Q1 - 1.2 * IQR
+          upper_bound = Q3 + 1.2 * IQR
           return series[(series >= lower_bound) & (series <= upper_bound)]
 
     column_groups = {
